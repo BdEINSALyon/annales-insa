4e5e6cf010e682ab254b91f9e59d07056ed97bcf
pmdartus <dartus.pierremarie@gmail.com>
2013-08-31 14:55:01 +0200

    Default value for document

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,4 +1,6 @@
 class Document < ActiveRecord::Base
+	before_save :default_value
+
 	belongs_to :category
 	has_many :pages
 	mount_uploader :original_file, DocumentUploader
@@ -6,4 +8,8 @@ class Document < ActiveRecord::Base
 	DEPARTEMENT = ['PC', 'BIO', 'BIM', 'GCU', 'GE', 'GEN', 'GMC', 'GMD', 'GMPP', 'GI', 'IF', 'TC']
 	validates_inclusion_of :departement, in: DEPARTEMENT
 	validates_presence_of :title, :scholar_year, :original_file, :departement, :publication_year, :validated
+
+	def default_value
+		self.validated ||= false
+	end
 end
