8ba52393e249372132f9e4592de85e3d276d6095
pmdartus <dartus.pierremarie@gmail.com>
2013-08-31 15:25:22 +0200

    Create  document controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 AnnalesInsa::Application.routes.draw do
   devise_for :users
+
+  # If current_user is authenticated
+  authenticated :user do
+    root 'documents#index', as: 'authenticated_root'
+  end
+
+  # Public homepage
   root to: "welcome#index"
 end
