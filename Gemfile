64f7a98c256390c364cc2412f0eff70e1f86ff67
pmdartus <dartus.pierremarie@gmail.com>
2013-08-31 10:25:39 +0200

    Modif Gemfile

---
+++ Gemfile
@@ -0,0 +1,28 @@
+source 'https://rubygems.org'
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '4.0.0'
+
+# Use sqlite3 as the database for Active Record
+gem 'sqlite3'
+
+# Assets pipeline
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+
+# Json builder
+gem 'jbuilder', '~> 1.2'
+
+  # bundle exec rake doc:rails generates the API under doc/api.
+group :doc do
+  gem 'sdoc', require: false
+end
+
+# Deployement and production
+gem 'capistrano', group: :development
+group :production do
+	gem 'unicorn'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AnnalesInsa::Application.config.secret_key_base = '9616a211336520db7b707cd2d2b2fac4034bf7ff03d3b21521533093e7c605e2fa6d8e40da4823ccb7a185823df0b6bc8d853ef1d0a38b5f14f2efed5c438bc4'
